39d2f703ee3a29809030506af13a96db5ca94142
MalekBenJemia <malekbenjemia@gmail.com>
2021-02-11 00:55:15 -0500

    able to collect the data and console.log the employees

--- app.js
+++ app.js
@@ -1,62 +1,18 @@
 const fs = require('fs');
-const inquirer = require('inquirer');
+const Page= require('./lib/Page.js');
 
+//initial function
+new Page().initializePage();
 
-
-const promptUser = () => {
-    return inquirer.prompt([
-      
-        {
-            type: 'input',
-            name: 'description',
-            message: 'ok',
-            validate: nameInput => {
-              if (nameInput) {
-                return true;
-              } else {
-                return false;
-              }
-            }
-        },
-       
-        {
-            type: 'checkbox',
-            name: 'license',
-            message: 'ok',
-            choices: ['None', 'Apache License 2.0'],
-            validate: nameInput => {
-                if (nameInput.length==1) {
-                  return true;
-                } else {
-                  return false;
-                }
-              }
-        },
-        
-
-    ]);
-};
-
-const generateIndex = () => {};
-
-// TODO: Create a function to write README file
+//write file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            console.log(err);
-            return;
-          }
-          console.log('HTML page created successfully!');
-        });
+  fs.writeFile(fileName, data, err => {
+      if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('HTML page created successfully!');
+      });
 };
 
-// TODO: Create a function to initialize app
-function init() {
-    promptUser()
-    .then(answer => {
-        return writeToFile('./dist/index.html',generateIndex(answer));
-      })
-}
-
-// Function call to initialize app
-init();
+    
